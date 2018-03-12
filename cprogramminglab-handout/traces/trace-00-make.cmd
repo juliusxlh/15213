51c9acdc73ac25e511748ae09471f818e96bdcdb
Xu <zjuxlh1993@gmail.com>
2018-03-12 09:47:59 -0700

    test cmake 1 fixed

--- cprogramminglab-handout/traces/trace-00-make.cmd
+++
@@ -1,7 +0,0 @@
-#this trace is never called.  Instead the function runMake in driver.py is executed
-#
-#below is a list of commands that are executed in driver.py
-#---------------------------------------------------------
-#make
-#touch queue.c
-#make
