f1582358a472fac544aab5554fe44b60fcdf10d0
list <zjuxlh1993@gmail.com>
2018-03-13 06:04:29 -0700

    free

--- lab0/cprogramminglab-handout/queue.c
+++ lab0/cprogramminglab-handout/queue.c
@@ -23,10 +23,11 @@
 queue_t *q_new()
 {
     queue_t *qn= (queue_t*) malloc(sizeof(queue_t));
+    qn->size = (int *) malloc(sizeof(int));
     if (!qn) return NULL;
     qn->head = NULL;
     qn->tail = NULL;
-    qn->size = 0;
+    *(qn->size) = 0;
     /* Remember to handle the case if malloc returned NULL */
     return qn;
 }
@@ -42,7 +43,10 @@ void q_free(queue_t *q)
         free(nowp);
         nowp=nextp;
     }
+    free(q->size);
+    q->size = NULL;
     free(q);
+    q = NULL;
 }
 
 /*
@@ -61,8 +65,8 @@ bool q_insert_head(queue_t *q, int v)
     insp->value = v;
     insp->next = q->head;
     q->head = insp;
-    ++(q->size);
-    if (q->size == 1) q->tail = insp;
+    ++*(q->size);
+    if (*(q->size) == 1) q->tail = insp;
     return true;
 }
 
@@ -79,13 +83,15 @@ bool q_insert_tail(queue_t *q, int v)
     /* Remember: It should operate in O(1) time */
     if (!insp) return false;
     insp->value = v;
-    q->tail->next = insp;
-    q->tail = insp;
-    ++(q->size);
-    if (q->size == 1) q->head = insp;
-    return false;
+    insp->next = NULL;
+    if (q->size){
+        q->tail->next = insp;
+        q->tail = insp;
+    } else q->tail = insp;
+    ++*(q->size);
+    if (*(q->size) == 1) q->head = insp;
+    return true;
 }
-
 /*
   Attempt to remove element from head of queue.
   Return true if successful.
@@ -96,10 +102,12 @@ bool q_insert_tail(queue_t *q, int v)
 bool q_remove_head(queue_t *q, int *vp)
 {
     if (!q||!(q->head)) return false;
-    if (!vp) *vp = q->head->value;
+    if (vp) *vp = q->head->value;
     list_ele_t *delp = q->head;
     q->head = q->head->next;
+    --*(q->size);
     free(delp);
+    delp = NULL;
     return true;
 }
 
@@ -110,7 +118,8 @@ bool q_remove_head(queue_t *q, int *vp)
 int q_size(queue_t *q)
 {
     /* Remember: It should operate in O(1) time */
-    return (!q?0:q->size);
+    if (!q) return 0;else
+    return *(q->size);
 }
 
 /*
@@ -123,17 +132,18 @@ int q_size(queue_t *q)
 void q_reverse(queue_t *q)
 {
     if (q) {
-        if ((q->head)){
+        if (q->head){
             q->tail = q->head;
             list_ele_t *p1 = q->head;
             list_ele_t *p2 = p1->next;
             while (p2){
-                list_ele_t *p3 = p1;
+                list_ele_t *p3 = p2->next;
+                p2->next = p1;
                 p1 = p2;
-                p2->next = p3;
-                p2 = p1->next;
+                p2 = p3;
             }
             q->head = p1;
+            q->tail->next = NULL;
             }
     }
 }
