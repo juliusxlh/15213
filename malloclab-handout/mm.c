ebb9dedf29bc67fe11e2a39b31639ce8faab5a45
zjuxlh1993 <ez_xlh@126.com>
2018-09-16 09:54:54 -0700

    define,mm_malloc,mm_init

--- malloclab-handout/mm.c
+++ malloclab-handout/mm.c
@@ -35,21 +35,56 @@ team_t team = {
     ""
 };
 
-/* single word (4) or double word (8) alignment */
-#define ALIGNMENT 8
+#define MAX(x, y) ((x) > (y)? (x) : (y))
+#define PACK(size, alloc) ((size) | (alloc))
+#define WSIZE 4// word = 4 bytes
+#define DSIZE 8// 2 * word = 8 bytes
+#define GET(p) (*(unsigned int *)p)
+#define PUT(p, val) (*(unsigned int*)p = (val))
+#define GET_SIZE(p) (GET(p) & ~0X7)
+#define GET_ALLOC(p) (GET(p) & 0x1)
+#define HDRP(bp) ((char *)(bp) - WSIZE)
+#define FTRP(bp) ((char *)(bp) + GET_SIZE(HDRP(bp)) - DSIZE)
+#define NEXT_BLKP(bp) ((char *)(bp) + GET_SIZE(HDRP(bp)))
+#define PREV_BLKP(bp) ((char *)(bp) - GET_SIZE((char *)(bp) - DSIZE))
+#define CHUNKSIZE (1<<12)
 
-/* rounds up to the nearest multiple of ALIGNMENT */
-#define ALIGN(size) (((size) + (ALIGNMENT-1)) & ~0x7)
+void* heap_list;
+int mm_init(void);
+bool mm_check();
+void * mm_malloc(size_t);
+void mm_free(void *);
+void *find_fit(size_t);
+void *extend_heap(size_t);
+void *place(void*, size_t);
+void *mm_realloc(void *, size_t);
 
-
-#define SIZE_T_SIZE (ALIGN(sizeof(size_t)))
+/*
+*/
+void *find_fit(size_t size)
+{
+}
 
 /* 
  * mm_init - initialize the malloc package.
  */
 int mm_init(void)
 {
+    void *heap_list = mem_sbrk(4);
+    PUT(heap_list, 0);
+    PUT(heap_list + (1*WSIZE), PACK(DSIZE, 1));
+    PUT(heap_list + (2*WSIZE), PACK(DSIZE, 1));
+    PUT(heap_list + (3*WSIZE), PACK(0, 1));
+    heap_list += (2*WSIZE);
+    if (extend_heap(CHUNKSIZE/WSIZE) == NULL) return -1;
     return 0;
+}
+
+/*
+    return true if valic else return false;
+*/
+bool mm_check()
+{
 }
 
 /* 
@@ -58,14 +93,17 @@ int mm_init(void)
  */
 void *mm_malloc(size_t size)
 {
-    int newsize = ALIGN(size + SIZE_T_SIZE);
-    void *p = mem_sbrk(newsize);
-    if (p == (void *)-1)
-	return NULL;
-    else {
-        *(size_t *)p = size;
-        return (void *)((char *)p + SIZE_T_SIZE);
+    char* bp;
+    size_t newsize = (size + DSIZE + DSIZE-1) / WSIZE;
+    bp = find_fit(newsize);
+    // if the heap donit have enought memory, get more memory
+    if (bp == NULL){
+        size_t extend_heap_size = MAX(newsize, CHUNKSIZE);
+        bp = extend_heap(extend_heap_size/WSIZE);
+        if (bp == NULL) return NULL;
     }
+    place(bp, newsize);
+    return bp;
 }
 
 /*
